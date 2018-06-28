b1b3c6916b8cd7332a4ef956836c46b1d6273128
Bryan Zarzuela <bryan@teleserv.ph>
2018-06-28 12:55:21 +0800

    Holidays are now factored in to the calculation

--- src/DeadlineCalculator.php
+++ src/DeadlineCalculator.php
@@ -2,10 +2,22 @@
 
 namespace Bzarzuela\DeadlineCalculator;
 
+use Illuminate\Support\Collection;
+
 class DeadlineCalculator
 {
     protected $startFrom = null;
     protected $tat = null;
+    protected $holidays;
+
+    /**
+     * DeadlineCalculator constructor.
+     */
+    public function __construct()
+    {
+        $this->holidays = new Collection();
+    }
+
 
     public function startFrom($timestamp)
     {
@@ -23,7 +35,21 @@ class DeadlineCalculator
 
     public function deadline()
     {
-        return date('Y-m-d H:i:s', $this->startFrom + $this->tat);
+        $deadline = $this->startFrom + $this->tat;
+
+        foreach ($this->holidays as $holiday) {
+            $holiday_timestamp = strtotime($holiday);
+            if (($holiday_timestamp >= $this->startFrom) and ($holiday_timestamp <= $deadline)) {
+                $deadline += 86400;
+            }
+        }
+
+        return date('Y-m-d H:i:s', $deadline);
+    }
+
+    public function addHoliday($holiday)
+    {
+        $this->holidays->push($holiday);
     }
 
 
--- tests/DeadlineCalculationTest.php
+++ tests/DeadlineCalculationTest.php
@@ -13,6 +13,34 @@ class DeadlineCalculationTest extends TestCase
         $calculator = new DeadlineCalculator();
         $calculator->startFrom('2018-06-28 12:20:00');
         $calculator->tatInDays(3);
+
+        $this->assertEquals('2018-07-01 12:20:00', $calculator->deadline());
+    }
+
+    /** @test */
+    function holidays_can_be_skipped()
+    {
+        $calculator = new DeadlineCalculator();
+
+        $calculator->startFrom('2018-06-28 12:20:00');
+        $calculator->tatInDays(3);
+
+        $calculator->addHoliday('2018-06-29');
+
+        $this->assertEquals('2018-07-02 12:20:00', $calculator->deadline());
+    }
+
+    /** @test */
+    function holidays_that_do_not_affect_the_deadline_are_ignored()
+    {
+        $calculator = new DeadlineCalculator();
+
+        $calculator->startFrom('2018-06-28 12:20:00');
+        $calculator->tatInDays(3);
+
+        $calculator->addHoliday('2018-06-27');
+        $calculator->addHoliday('2018-07-02');
+
         $this->assertEquals('2018-07-01 12:20:00', $calculator->deadline());
     }
 }
